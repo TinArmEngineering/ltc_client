e85854b3aeffe32727ea0d5cee27e70bf1f5c146
Martin West <martin@tinarmengineering.com>
2024-04-12 20:13:50 +0200

    added job materials to examples

--- tinarm/helpers.py
+++ tinarm/helpers.py
@@ -4,11 +4,26 @@ import requests
 
 
 class Machine(object):
-    def __init__(self, stator, rotor, winding):
+    def __init__(self, stator, rotor, winding, materials=None):
 
         self.stator = stator
         self.rotor = rotor
         self.winding = winding
+        if materials is not None:
+            self.materials = materials
+        else:
+            self.materials = {
+                "rotor_lamination": "66018e5d1cd3bd0d3453646f",  # default M230-35A
+                "rotor_magnet": "66018e5b1cd3bd0d3453646c",  # default is N35UH
+                "rotor_air_L": "6602fb42c4a87c305481e8a6",
+                "rotor_air_R": "6602fb42c4a87c305481e8a6",
+                "rotor_banding": "6602fb42c4a87c305481e8a6",
+                "stator_lamination": "66018e5d1cd3bd0d3453646f",  # default M230-35A
+                "stator_slot_wedge": "6602fb7239bfdea291a25dd7",
+                "stator_slot_liner": "6602fb5166d3c6adaa8ebe8c",
+                "stator_slot_winding": "66018e5d1cd3bd0d34536470",
+                "stator_slot_potting": "6602fd41b8e866414fe983ec",
+            }
 
     def __repr__(self) -> str:
         return f"Machine({self.stator}, {self.rotor}, {self.winding})"
@@ -78,6 +93,7 @@ class Job(object):
             "type": self.type,
             "tasks": 11,
             "data": [],
+            "materials": [],
         }
 
         operating_point_api = [
@@ -91,9 +107,14 @@ class Job(object):
             NameQuantityPair("simulation", k, Quantity(*self.simulation[k].to_tuple()))
             for k in self.simulation
         ]
+
         job["data"].extend(list(x.to_dict() for x in operating_point_api))
         job["data"].extend(list(x.to_dict() for x in simulation_api))
         job["data"].extend(self.machine.to_api())
+        job["materials"] = [
+            {"part": key, "material_id": value}
+            for key, value in self.machine.materials.items()
+        ]
         return job
 
     def run(self):
