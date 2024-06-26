a48036ec958565b9f4b8209660cf12c456b88e10
Martin West <martin@tinarmengineering.com>
2024-06-26 02:01:24 +0200

    updated version for release

--- ltc_client/__init__.py
+++ ltc_client/__init__.py
@@ -5,7 +5,7 @@ from ltc_client.api import Api, NameQuantityPair, Quantity, Unit
 from ltc_client.helpers import Machine, Job, decode
 
 __title__ = "TINARM - Node creation tool for TAE workers"
-__version__ = "0.1"
+__version__ = "0.2.3"
 __author__ = "Martin West, Chris Wallis"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2023 Tin Arm Engineering Ltd."
